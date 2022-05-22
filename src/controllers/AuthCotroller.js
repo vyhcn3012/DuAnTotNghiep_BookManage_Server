6a0c9587183bb58f77fff1bbac3900870f8510c8
hoangvy <81845721+vyhcn3012@users.noreply.github.com>
2022-05-22 11:20:29 +0700

    AuthCotroler

--- src/controllers/AuthCotroller.js
+++ src/controllers/AuthCotroller.js
@@ -1,7 +1,10 @@
 const { AuthService } = require('./../services/AuthService');
+const authService = new AuthService(new Auth().getInstance(), new User().getInstance());
 const { Auth } = require('./../models/Auth');
+const { User } = require('./../models/User');
 const autoBind = require('auto-bind');
-
+const { OAuth2Client } = require('google-auth-library'), client = new OAuth2Client(config.GOOGLE_CLIENT_ID)
+const config = require('../../config/config').getConfig();
 class AuthCotroller {
     constructor(service) {
         this.service = service;
@@ -10,7 +13,30 @@ class AuthCotroller {
 
     async login(req, res, next) {
         try{
-            
+            const { token, unit, token_fcm, } = req.body;
+            const ticket = await client.verifyIdToken({
+                idToken: token,
+                audience: config.GOOGLE_CLIENT_ID
+            });
+            const { name, email, picture } = ticket.getPayload();
+            const check_email = config.EMAIL_GOOGLE_TESTING;
+            if(check_email == email){
+                const body = {
+                    email: email,
+                    role: config.USER_ROLE,
+                    name: name,
+                    image: picture,
+                    phone: '0919560820',
+                    token_fcm: token_fcm,
+                }
+                const response = await authService.login(body);
+                await res.status(response.statusCode).json(response);
+            }
+        }catch(e) {
+            console.log('>>>>>>132 login error: ' + e);
+            next(e);
         }
     }
 }
+
+module.exports = new AuthController(authService);
