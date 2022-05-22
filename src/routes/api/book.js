c0246c02733e1d509c926b4d0a92c74f3016917e
hohoangphuc1907 <84660182+hohoangphuc1907@users.noreply.github.com>
2022-05-22 16:21:06 +0700

    phuc

--- src/routes/api/book.js
+++ src/routes/api/book.js
@@ -2,8 +2,6 @@ const bookController = require('../../controllers/BookController');
 
 const express = require('express'),
     router = express.Router();
-const express = require('express'),
-    router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Welcome to the books')
