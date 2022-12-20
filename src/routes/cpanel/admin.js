853f138e7108c5341d6114bc1d64fa250aa9c246
Hoang Cong Nhut Vy <81845721+vyhcn3012@users.noreply.github.com>
2022-12-20 14:55:41 +0700

    n

--- src/routes/cpanel/admin.js
+++ src/routes/cpanel/admin.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const AuthCotroller = require('../../controllers/AuthCotroller');
+const CategoryController = require('../../controllers/CategoryController');
 
 router = express.Router();
+
+//Manager User
 router.get(
     '/quan-ly-nguoi-dung/:id',
     AuthCotroller.checkLogin,
     AuthCotroller.indexUser_Cpanel,
+);
+
+//Manage Category
+router.get(
+    '/quan-ly-danh-muc',
+    AuthCotroller.checkLogin,
+    CategoryController.getAllCategories_Cpanel,
 );
 
 module.exports = router;
