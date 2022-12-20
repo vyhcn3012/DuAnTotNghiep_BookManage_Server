5ec208bcab77823ce8d299253193143594a6123a
Hoang Cong Nhut Vy <81845721+vyhcn3012@users.noreply.github.com>
2022-12-20 21:13:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ io.on('connection', (socket) => {
 
     socket.on('send-msg', (data) => {
         const sendUserSocket = onlineUsers.get(data.to);
+        console.log(data.msg);
         if (sendUserSocket) {
             socket.to(sendUserSocket).emit('msg-recieve', data.msg);
         }
