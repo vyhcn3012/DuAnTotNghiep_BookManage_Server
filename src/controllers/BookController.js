7cd214d1688696186ffd97d1b1b7eae61c3b5ee3
hohoangphuc1907 <84660182+hohoangphuc1907@users.noreply.github.com>
2022-12-22 23:06:53 +0700

    update

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -23,15 +23,7 @@ class BookController extends Controller {
     async getBooks(req, res, next) {
         try {
             const response = await this.service.getAll({ limit: 1000 });
-            let data = response.data;
-            data = data.map((item) => {
-                return {
-                    ...item,
-                    view: Math.floor(Math.random() * 100) + 1,
-                    evaluate: Math.floor(Math.random() * 5) + 1,
-                };
-            });
-            await res.status(response.statusCode).json(data);
+            await res.status(response.statusCode).json(response);
         } catch (e) {
             // next(e);
         }
